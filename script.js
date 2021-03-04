dc0ab27e8637dd9910881e4b4c65cab3e4c89d2c
Jorge R <jorgebeto00@gmail.com>
2021-03-03 19:07:39 -0600

    modified-css

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 window.onload = function () {
-    let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-    let symbol = ["King", "Queen", "Jack", "Ace"];
+    let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "King", "Queen", "Jack", "Ace"];
+    let symbol = ['♦', '♥', '♠', '♣'];
 
     let result1 = Math.floor(Math.random() * number.length);
     let result2 = Math.floor(Math.random() * symbol.length);
 
     let num;
+    let sym;
+    let symColor;
+
     for(let i = 0; i < number.length; i++){
         if(i == result1){
             num = number[i];
         }
     }
+    for(let i = 0; i < symbol.length; i++){
+        if(i == result2){
+            sym = symbol[i];
+        }
+        if(sym == '♥' || sym == '♦'){
+            symColor = 'red';
+        }
+    }
 
+    
+    let newSymbol = document.querySelector(".symbol");
+    let newNumber = document.querySelector(".letter");
+    let newSymbol2 = document.querySelector(".symbol2");
+    newSymbol.innerHTML = sym;
+    newNumber.innerHTML = num;
+    newSymbol2.innerHTML = sym;
+    newSymbol.style.color = symColor;
+    newSymbol2.style.color = symColor;
 
-    let newDiv = document.createElement("P");
-    let newDiv2 = document.createElement("P");
-    let newDiv3 = document.createElement("P");
-    newDiv.innerHTML = "hello";
-    newDiv2.innerHTML = num;
-    newDiv3.innerHTML = result2;
-    document.body.appendChild(newDiv);
-    document.body.appendChild(newDiv2);
-    console.log(num);
+    
 };
